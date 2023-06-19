4445d8f941610de275177074489d4a3ad9c53322
WT-MM <wesley.maa@gmail.com>
2023-06-18 21:18:42 -0700

    prompt formatting

---
+++ SDEval/formatPrompts.py
@@ -0,0 +1,24 @@
+import re
+import json 
+
+
+final = {}
+
+
+with open("raw.txt", "r", encoding="utf-8") as f:
+    bigstring = f.read()
+    #Strip newlines
+    bigstring = bigstring.replace("\n", "")
+    biglist = bigstring.split("-")
+    for thing in biglist:
+        print(thing)
+        print("-----")
+        category, prompts = thing.split(":")
+        prompts = list(filter(None, map(str.rstrip, re.split('\w\.\s', prompts))))
+
+        final[category] = prompts
+
+print(final)
+
+with open("prompts.json", "w", encoding="utf-8") as f:
+    json.dump(final, f, indent=4, ensure_ascii=False)
